5638e234c8620237fd0146564e49ce461c0f318c
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-06-10 01:26:43 +0500

    Логирование запросов

--- src/Trivium.TestTask/Dictionaries/Program.cs
+++ src/Trivium.TestTask/Dictionaries/Program.cs
@@ -1,5 +1,21 @@
+﻿using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Логирование запросов.
+builder.Services.AddHttpLogging(optoins =>
+{
+    optoins.LoggingFields =
+        HttpLoggingFields.RequestPath |
+        HttpLoggingFields.RequestQuery |
+        HttpLoggingFields.RequestMethod |
+        HttpLoggingFields.ResponseStatusCode |
+        HttpLoggingFields.ResponseBody;
+    optoins.CombineLogs = true;
+});
+
 var app = builder.Build();
+
+app.UseHttpLogging();
 
 app.Run();
