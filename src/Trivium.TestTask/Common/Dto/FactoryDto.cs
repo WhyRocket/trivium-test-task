84bb0c09f565d3026fdfed088328d8c439238b53
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-06-16 16:52:34 +0500

    Проект Common

---
+++ src/Trivium.TestTask/Common/Dto/FactoryDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Common.Dto;
+
+public class FactoryDto : EntityDto
+{
+    [JsonPropertyName("region")]
+    public string Region { get; set; } = null!;
+
+    [JsonPropertyName("year")]
+    public int Year { get; set; }
+}
